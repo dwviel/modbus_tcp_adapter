c574b9c97b4f1ecef82d54b65685b3069c0c5066
David <dwviel@yahoo.com>
2018-09-20 09:44:43 -0400

    add main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+/**
+ *  ModbusTCP adapter for ControlMQ.
+ *  Interposes on adapter platform between ModbusTCP endpoint
+ *  and network.  Deployed in pairs, onne on each end of network,
+ *  such that the two adapters convert the ModbusTCP message to 
+ *  ControlMQ format, send that over the network, and then
+ *  convert it back to ModbusTCP format for delivery to the 
+ *  receiving device.
+ */
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+
+}
